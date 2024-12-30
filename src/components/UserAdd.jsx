f6d5f9eaa69c2683785128c73d66e30ea4db8821
sajal03593 <shihabuddinsajal78@gmail.com>
2024-12-30 17:43:13 +0600

    m

--- src/components/UserAdd.jsx
+++ src/components/UserAdd.jsx
@@ -29,7 +29,7 @@ const UserAdd = () => {
 
       //  headers:{'Content-Type':'multipart/form-data'}
 
-        axios.post('https://myrent-bcknd-9adzs6yk3-shihab-uddins-projects-788c87a9.vercel.app/userAdd', formData).then((response) => {
+        axios.post('https://myrent-bcknd-9adzs6yk3-shihab-uddins-projects-788c87a9.vercel.app/UserAdd', formData).then((response) => {
             console.log(response);
             toast.success('User Added successfully');
         }).catch(error => {
