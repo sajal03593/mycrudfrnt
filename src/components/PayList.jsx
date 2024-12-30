b6394170ddc14c466d3926b574e9b099574077f7
sajal03593 <shihabuddinsajal78@gmail.com>
2024-12-30 21:16:54 +0600

    m

--- src/components/PayList.jsx
+++ src/components/PayList.jsx
@@ -145,7 +145,7 @@ const PayList = () => {
                                     <td>{item.dueRnt}</td>
                                     <td>{item.payStatus}</td>
                                     <td>{item.payDate}</td>
-                                    <td>{item.payMonth}</td>
+                                    <td>{item.payMonth.getMonth()}</td>
                                     <td>
                                         <button type="button" onClick={() => {
                                             onOpenModal(item)
